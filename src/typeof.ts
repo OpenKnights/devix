456fc5544e790341cb1bb980d666fc507f9ef578
Coder-King3 <2196592083@qq.com>
2024-04-22 16:02:51 +0800

    🚀feat: Add more type checks in the isType function.

--- src/typeof.ts
+++ src/typeof.ts
@@ -1,26 +1,34 @@
 export default function getDataType(target: any) {
   const type = typeof target
-  return type === 'object'
+  return type != 'object'
     ? type
     : Object.prototype.toString.call(target).slice(8, -1).toLowerCase()
 }
 
 const typeCheckers: { [key: string]: (target: any) => boolean } = {
-  string: (target) => typeof target === 'string',
-  number: (target) => typeof target === 'number',
-  boolean: (target) => typeof target === 'boolean',
-  null: (target) => target === null,
+  // DataType
   undefined: (target) => typeof target === 'undefined',
+  null: (target) => target === null,
+  number: (target) => typeof target === 'number',
+  string: (target) => typeof target === 'string',
+  boolean: (target) => typeof target === 'boolean',
   symbol: (target) => typeof target === 'symbol',
   bigint: (target) => typeof target === 'bigint',
+
+  // ObjectType
   object: (target) => target !== null && typeof target === 'object',
   array: (target) => Array.isArray(target),
+  date: (target) => target instanceof Date,
   function: (target) => typeof target === 'function',
   set: (target) => target instanceof Set,
   map: (target) => target instanceof Map,
-  date: (target) => target instanceof Date,
-  regexp: (target) => target instanceof RegExp
+  regexp: (target) => target instanceof RegExp,
+  promise: (target) => target instanceof Promise
 }
 
-export const isType = (type: string, target: any) =>
-  typeCheckers[type]?.(target) || false
+export const isType = (type: string, target: any) => {
+  const dataType = typeCheckers[type]
+    ? typeCheckers[type](target)
+    : getDataType(target) === type
+  return dataType
+}
