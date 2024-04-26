46cd2be8f5d9ac8616cdc7ed62c716d2c26d65ba
Coder-King3 <2196592083@qq.com>
2024-04-27 01:16:23 +0800

    ♻️refactor: Standardize the types file and rename it to types/index.d.ts.

---
+++ types/index.d.ts
@@ -0,0 +1,66 @@
+import type Koa from 'koa'
+
+//! format types
+export interface ITimerObj {
+  year: string
+  month: string
+  day: string
+  hours: string
+  minutes: string
+  seconds: string
+  week: string
+  weekNum: string
+}
+
+export type TFormatTimer = (
+  cellValue: string | number | Date,
+  formatType?: string
+) => string | ITimerObj
+
+//! retalimit types
+export type ThrottleOptions = {
+  leading?: boolean
+  trailing?: boolean
+}
+
+//! test types
+export interface ITestServer extends Koa {
+  http?: any
+  callback: () => any
+  close?: () => void
+  port?: number
+  url?: string
+  listen: () => Promise<any> | any
+}
+
+interface TestServerOption {
+  url: string
+  method: string
+  handler: (
+    ctx: Koa.ParameterizedContext<
+      any,
+      Router.IRouterParamContext<any, {}>,
+      any
+    >,
+    next: Koa.Next
+  ) => any
+}
+
+export interface ITestServerOptions {
+  routes?: Array<TestServerOption>
+}
+
+//! typeof Types
+// utils types
+export interface ITypeCheckes {
+  [key: string]: (target: any) => boolean
+}
+export type TIsType = (type: string, target: any) => boolean
+
+//! other types
+export type Tcase = 'upper' | 'lower'
+export type TCases = [Tcase, Tcase]
+export type TTransGetParams = (params: IDataObject) => string
+export interface IDataObject {
+  [key: string]: any
+}
