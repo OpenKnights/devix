ed3e1398c25d382c7b2377051bf64ca6ede4715f
Coder-King3 <2196592083@qq.com>
2024-04-27 01:29:12 +0800

    🚀feat: Add the createTestServer method along with its test cases.

--- example/index.ts
+++ example/index.ts
@@ -1,6 +1,7 @@
 import getDataType, { isType } from '../src/typeof'
 import { deepClone } from '../src/clone'
-import {stringCase} from '../src/others'
+import { stringCase } from '../src/others'
+import createTestServer from '../src/server'
 
 //! isType
 const myName = 'King-3'
@@ -34,9 +35,35 @@ console.log(
   obj1.info.count === obj3.info.count
 )
 
-
 //! stringCase
-const dx1 = stringCase('load report',[' ',''],['lower','upper'])
+const dx1 = stringCase('load report', [' ', ''], ['lower', 'upper'])
 console.log(`dx1:`, dx1)
 
+//! test server
+const routes = [
+  {
+    url: '/testGet',
+    method: 'get',
+    handler: async (ctx: any, next: any) => {
+      ctx.body = {
+        code: 200,
+        data: {},
+        message: `POST 接口测试: /testPost`
+      }
+    }
+  }
+]
 
+let server = await createTestServer({ routes })
+const prefix = (api: string) => `${server.url}${api}`
+console.log(`createTestServer url:`, server.url)
+
+fetch(prefix('/testGet'))
+  .then((res) => res.json())
+  .then((res) => {
+    console.log(`res:`, res)
+  })
+
+setTimeout(() => {
+  server.close()
+}, 10000)
---
+++ src/server.ts
@@ -0,0 +1,44 @@
+import http from 'http'
+import Koa from 'koa'
+import Router from 'koa-router'
+import json from 'koa-json'
+import bodyparser from 'koa-bodyparser'
+import { ITestServer, ITestServerOptions } from '../types'
+
+const createTestServer = (options: ITestServerOptions = {}) => {
+  const server: ITestServer = new Koa()
+  const router: any = new Router()
+  server.use(bodyparser({ enableTypes: ['json', 'form', 'text'] }))
+  server.use(json({}))
+
+  if (options.routes) {
+    options.routes.forEach((route) => {
+      router[route.method](route.url, route.handler)
+    })
+    server.use(router.routes())
+  }
+
+  server.http = http.createServer(server.callback())
+
+  server.listen = () =>
+    new Promise((resolve) => {
+      server.http.listen()
+      resolve(server)
+    }).then(() => {
+      server.port = server.http.address().port
+      server.url = `http://localhost:${server.port}`
+    })
+
+  server.close = () =>
+    new Promise((resolve) => {
+      server.http.close()
+      resolve(server)
+    }).then(() => {
+      server.port = undefined
+      server.url = undefined
+    })
+
+  return server.listen().then(() => server)
+}
+
+export default createTestServer
